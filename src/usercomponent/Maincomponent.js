111d023901e72fd8f9485793f8a7d0ac571e7444
poojagirne <poojagirne24@gmail.com>
2021-06-06 22:01:07 +0530

    first commit

---
+++ src/usercomponent/Maincomponent.js
@@ -0,0 +1,35 @@
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+import loader from './loader1.gif'	
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter,Switch,Route,Link as NavLink} from 'react-router-dom'
+import {Dropdown} from 'react-bootstrap'
+import Navbar from './navbar'
+import Userdetails from './Userdetails'
+import Firstname from './Firstname'
+import Lastname from './Lastname'
+import None from './None'
+function Maincomponent()
+{
+
+return(
+  <div>
+    <Navbar/>
+          <Route exact path="/">
+				  <Userdetails/>
+				</Route>
+				  <Route exact path="/None">
+				  <None/>
+				</Route>
+        <Route exact path="/Firstname">
+				  <Firstname/>
+				</Route>
+        <Route exact path="/Lastname">
+				  <Lastname/>
+				</Route>
+
+  </div>
+)
+
+}
+export default Maincomponent;
